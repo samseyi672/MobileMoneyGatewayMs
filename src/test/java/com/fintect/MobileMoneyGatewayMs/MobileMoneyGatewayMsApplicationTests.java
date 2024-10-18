b68a983788cb4c1fc55a3d03607904f50b8e8c22
samseyi672 <samseyi672@gmail.com>
2024-10-18 05:17:15 +0100

    first commit

---
+++ src/test/java/com/fintect/MobileMoneyGatewayMs/MobileMoneyGatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintect.MobileMoneyGatewayMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileMoneyGatewayMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
